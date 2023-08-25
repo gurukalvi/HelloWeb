c67ff61edaa4076b227f8a7b209263943bcbfd8e
Prasath D <prasathmd@gmail.com>
2023-08-25 22:06:09 +0530

    25-8-2023

--- TestWeb/src/main/java/com/web/MyHttpServlet.java
+++ TestWeb/src/main/java/com/web/MyHttpServlet.java
@@ -41,7 +41,7 @@ public class MyHttpServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		userCount++;
 		//name = manju
-		String username = request.getParameter("user_name");
+		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		
 		System.out.println("inside the doGet method");
--- TestWeb/src/main/java/com/web/jdbc/DBConnection.java
+++ TestWeb/src/main/java/com/web/jdbc/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         //connection URL username and password
-        Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb","root", "manju12345");
+        Connection connection= DriverManager.getConnection("jdbc:mysql://sql6.freemysqlhosting.net:3306/sql6641926","sql6641926", "pG7bX7JQLi");
         return connection;
     }
 }
