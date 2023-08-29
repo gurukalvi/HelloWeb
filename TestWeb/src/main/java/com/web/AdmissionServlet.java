a2a592efb78ae8bca7a13bd2ac72b230b93355a6
manjuiniyan <142672592+manjuiniyan@users.noreply.github.com>
2023-08-29 21:27:18 +0530

    Morning

--- TestWeb/src/main/java/com/web/AdmissionServlet.java
+++ TestWeb/src/main/java/com/web/AdmissionServlet.java
@@ -1,9 +1,9 @@
 package com.web;
-
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Set;
-
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -24,11 +24,39 @@ public class AdmissionServlet extends HttpServlet {
 		String email=req.getParameter("EmailID");
 		String cgpa=req.getParameter("CGPA");
 		String dob=req.getParameter("DOB");
-		System.out.println(" name="+name+" initial="+initial);
-
+		System.out.println(" name="+name+" initial="+initial+" studentid="+studentid+" rollnumber="+rollnumber+" qualification="+qualification+" mobilenumber="+mobilenumber+" email="+email+" cgpa="+cgpa+" dob="+dob);
+		  try (Connection connection = DriverManager.getConnection("jdbc:mysql://sql6.freemysqlhosting.net:3306/sql6641926", "sql6641926", "pG7bX7JQLi")) {
+			  String insert_query = "INSERT INTO `sql6641926`.`students` (`studentid`, `name`, `initial`, `rollnumber`, `Degree`, `Phone`, `Email`, `CGPA`, `DOB`) VALUES (?,?,?,?,?,?,?,?,?)";
+			  		
+	            try (PreparedStatement preparedStatement = connection.prepareStatement(insert_query)) {
+	                preparedStatement.setString(2, name);
+	                preparedStatement.setString(3, initial);
+	                preparedStatement.setString(4, rollnumber);
+	                preparedStatement.setString(5, qualification);
+	             	preparedStatement.setString(6, mobilenumber);
+	                preparedStatement.setString(7, email);
+	                preparedStatement.setString(8, cgpa);
+	                preparedStatement.setString(9, dob);
+	                preparedStatement.setString(1, studentid);
+	                
+	                int rowsAffected = preparedStatement.executeUpdate();
+	                if (rowsAffected >0) {
+	                	System.out.println("Successfuly inserted");
+	           
+	                    resp.getWriter().write("Update successful");
+	                } else {
+	                    resp.getWriter().write("Update failed");
+	                    System.out.println("failed inserted");
+	                }
+	    	            }
+		  }catch (SQLException e) {
+	            e.printStackTrace();
+	            resp.getWriter().write("An error occurred");
+	            System.out.println("Successfuly inserted");
+	        }
 		RequestDispatcher dispatcher = null;
-		dispatcher = req.getRequestDispatcher("MySchool.html");
+		dispatcher = req.getRequestDispatcher("StudentMarks.html");
 		dispatcher.forward(req, resp);
 	}
-
 }
+
